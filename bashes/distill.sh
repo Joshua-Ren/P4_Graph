1ffc76fe73101cd55c504c98d0f188aeadcdafd6
Joshua-Ren <renyi.joshua@gmail.com>
2022-10-07 16:26:58 -0400

    rndsmpdis

--- bashes/distill.sh
+++ bashes/distill.sh
@@ -24,5 +24,5 @@ cd /home/mila/y/yi.ren/P4_Graph/
 srun python /home/mila/y/yi.ren/P4_Graph/main_distill.py \
 --drop_ratio 0 \
 --proj_name P4_phase_observe --dataset_name ogbg-moltox21 --dis_loss ce_sample \
---backbone_type gcn --bottle_type upsample --ckp_name _best \
---run_name disbest_tox_gcn_upsample_cesmpedis
+--backbone_type gcn --bottle_type upsample --ckp_name _last \
+--run_name dislast_tox_gcn_upsample_cesmpedis
