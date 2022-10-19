d3174ae9863f56876ea383e9446a9a16877d4759
Joshua-Ren <renyi.joshua@gmail.com>
2022-10-19 12:38:41 -0400

    inital

--- bashes/resetrelearn.sh
+++ bashes/resetrelearn.sh
@@ -21,7 +21,7 @@ source /home/mila/y/yi.ren/env_graph/bin/activate
 
 cd /home/mila/y/yi.ren/P4_Graph/
 
-srun python /home/mila/y/yi.ren/P4_Graph/main_nil.py \
+srun python /home/mila/y/yi.ren/P4_Graph/main_resethead.py \
 --drop_ratio 0 --L 30 --V 20 \
 --proj_name P4_phase_observe --dataset_name ogbg-moltox21 \
 --backbone_type gcn --bottle_type updown --drop_ratio 0 \
