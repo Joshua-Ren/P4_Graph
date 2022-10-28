0191b9a2eff8cb0be3b60d6494b97dee3e613638
Joshua-Ren <renyi.joshua@gmail.com>
2022-10-28 17:06:27 -0400

    try reset

---
+++ bashes/nil_pcba_sweep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=long                           # Ask for unkillable job
+#SBATCH --cpus-per-task=2                                # Ask for 2 CPUs
+#SBATCH --gres=gpu:1                                     # Ask for 1 GPU
+#SBATCH --mem=10G                                        # Ask for 10 GB of RAM
+#SBATCH --job-name=nil-pcba
+#SBATCH --time=40:00:00                                   # The job will run for 3 hours
+#SBATCH --output=./logs/stage1.txt 
+
+# 1. Load the required modules
+module load python/3.8
+
+# 2. Load your environment
+source /home/mila/y/yi.ren/env_graph/bin/activate
+
+# 3. Copy your dataset on the compute node
+#cp /network/datasets/<dataset> $SLURM_TMPDIR
+
+# 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
+#    and look for the dataset into $SLURM_TMPDIR
+
+cd /home/mila/y/yi.ren/P4_Graph/
+
+wandb agent --count=2  joshuaren/P4_phase_observe/b88k4ng9
