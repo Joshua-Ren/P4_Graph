bf2f735ab4ca9037c46f3a559bc5f2c602d9c96a
Joshua-Ren <renyi.joshua@gmail.com>
2022-12-08 14:15:16 -0800

    sweep for extra distillation

--- bashes/nil_pcba_sweep.sh
+++ bashes/nil_pcba_sweep.sh
@@ -21,4 +21,4 @@ source /home/mila/y/yi.ren/env_graph/bin/activate
 
 cd /home/mila/y/yi.ren/P4_Graph/
 
-wandb agent --count=3 joshuaren/P4_phase_observe/h5ps343n
+wandb agent --count=2 joshuaren/P4_phase_observe/26a6936o
