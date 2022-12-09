1822c75f3bf619f109f8203eb56a0bfc6b749d8f
Joshua-Ren <renyi.joshua@gmail.com>
2022-12-08 22:40:40 -0800

    sweep for extra distillation

--- bashes/nil_hiv.sh
+++ bashes/nil_hiv.sh
@@ -23,8 +23,8 @@ cd /home/mila/y/yi.ren/P4_Graph/
 
 srun python /home/mila/y/yi.ren/P4_Graph/main_nil.py \
 --drop_ratio 0 \
---proj_name P4_phase_observe --dataset_name ogbg-molhiv --distill_set ogbg-molpcba \
+--proj_name P4_phase_observe --dataset_name ogbg-molhiv --distill_set ogbg-hiv \
 --backbone_type gcn --bottle_type upsample --drop_ratio 0.2 --L 123 --V 30 \
 --epochs_ft 100 --steps_dis 40000 --generations 10 --ft_lr 0.001 \
 --dis_loss ce_sample --dis_smp_tau 2 --teach_last_best best --dis_optim_type adam --dis_lr 0.001 \
---run_name hiv_distill_pcba
+--run_name hiv_distill_hiv_val
