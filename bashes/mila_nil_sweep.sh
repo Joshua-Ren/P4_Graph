a3a65fdc5326fb5987689f5e31717a633a82295c
Joshua-Ren <renyi.joshua@gmail.com>
2022-12-14 18:06:05 -0800

    inital

--- bashes/mila_nil_sweep.sh
+++ bashes/mila_nil_sweep.sh
@@ -21,4 +21,4 @@ source /home/mila/y/yi.ren/env_graph/bin/activate
 
 cd /home/mila/y/yi.ren/P4_Graph/
 
-wandb agent --count=5 joshuaren/P4_paper/rf4sfpax
+wandb agent --count=5 joshuaren/P4_paper/1akukghk
