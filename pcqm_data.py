95683c4e4cad319285a827d681734a4e094b029f
Joshua-Ren <renyi.joshua@gmail.com>
2023-01-11 14:00:36 -0800

    pcqm dataset

--- pcqm_data.py
+++ pcqm_data.py
@@ -1,4 +1,4 @@
 from ogb.lsc import PCQM4Mv2Dataset
 from ogb.utils import smiles2graph
 
-dataset = PCQM4Mv2Dataset(root = './', smiles2graph = smiles2graph)
+dataset = PCQM4Mv2Dataset(root = './dataset/', smiles2graph = smiles2graph)
