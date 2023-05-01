320e99c0dc74aca9ba997724e021ddcdfd428c07
Joshua-Ren <renyi.joshua@gmail.com>
2023-05-01 14:22:22 -0700

    beta-vae

--- main_generate_betavae.py
+++ main_generate_betavae.py
@@ -73,7 +73,6 @@ def kl_divergence(mu, logvar):
     return total_kld, dimension_wise_kld, mean_kld
 
 def main(args):
-    ALPHA_TABLE = [0.01, 0.1]#[1., 0.3, 0.1, 0.03, 0.01, 0.001]
     # ========== Generate seed ==========
     results = {'tloss':[],'vloss':[], 'dis_loss':[]}
     if args.seed==0:
@@ -81,46 +80,51 @@ def main(args):
     rnd_seed(args.seed)
 
     # ========== Prepare save folder and wandb ==========
+    seed_net = BetaVAE_H(z_dim=10,nc=3)
+    img_seed = None
+    # ========== Prepare the loader and optimizer
+    full_loader = generate_3dshape_fullloader_vae(args)
+    net = copy.deepcopy(seed_net)
+    net.to(args.device)
+    optimizer = optim.Adam(net.parameters(),lr=args.learning_rate)
+    for g in range(1):
+        for i,(x,_,reg,idx) in enumerate(full_loader):
+            x = x.float().to(args.device)
+            if img_seed is None:
+                img_seed = x[:8]
+            x_recon, mu, logvar = net(x)
+            recon_loss = reconstruction_loss(x, x_recon, args.recon_loss)
+            total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar)
+            beta_vae_loss = recon_loss + args.beta*total_kld
+            optimizer.zero_grad()
+            beta_vae_loss.backward()
+            optimizer.step()
+            # ------ Report to wandb
+            wandb.log({'idx_epoch':i})
+            wandb.log({'recon_loss':recon_loss.item()})
+            if i%600==0:
+                x_recon_seed, _, _ = net(img_seed)
+                images = wandb.Image(x_recon_seed.cpu().detach(), caption='epoch_'+str(i)+'_in_alpha_'+str(alpha))
+                wandb.log({"recon": images})
+    x_recon_seed, _, _ = net(img_seed)
+    images = wandb.Image(x_recon_seed.cpu().detach(), caption='epoch_'+str(i)+'_in_alpha_'+str(alpha))
+    wandb.log({"recon": images})     
+
+    save_name = 'bvae_alpha_'+str(alpha)+'.pth'
+    save_path = os.path.join(args.save_path, save_name)
+    torch.save(net.state_dict(),save_path)
+    #del full_loader
+if __name__ == '__main__':
+    ALPHA_TABLE = [0.01, 0.1]#[1., 0.3, 0.1, 0.03, 0.01, 0.001]
+    args = get_args_parser()
+    args = args.parse_args()
+    args.device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
+
     run_name = wandb_init(proj_name=args.proj_name, run_name=args.run_name, config_args=args)
     args.save_path = os.path.join('results','toy_betavae',run_name)
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
 
-    seed_net = BetaVAE_H(z_dim=10,nc=3)
-    img_seed = None
     for alpha in ALPHA_TABLE:
         args.sup_ratio = alpha
-        # ========== Prepare the loader and optimizer
-        full_loader = generate_3dshape_fullloader_vae(args)
-        net = copy.deepcopy(seed_net)
-        net.to(args.device)
-        optimizer = optim.Adam(net.parameters(),lr=args.learning_rate)
-        for g in range(1):
-            for i,(x,_,reg,idx) in enumerate(full_loader):
-                x = x.float().to(args.device)
-                if img_seed is None:
-                    img_seed = x[:8]
-                x_recon, mu, logvar = net(x)
-                recon_loss = reconstruction_loss(x, x_recon, args.recon_loss)
-                total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar)
-                beta_vae_loss = recon_loss + args.beta*total_kld
-                optimizer.zero_grad()
-                beta_vae_loss.backward()
-                optimizer.step()
-                # ------ Report to wandb
-                wandb.log({'idx_epoch':i})
-                wandb.log({'recon_loss':recon_loss.item()})
-                if i%600==0:
-                    x_recon_seed, _, _ = net(img_seed)
-                    images = wandb.Image(x_recon_seed.cpu().detach(), caption='epoch_'+str(i)+'_in_alpha_'+str(alpha))
-                    wandb.log({"recon": images})
-                
-        save_name = 'bvae_alpha_'+str(alpha)+'.pth'
-        save_path = os.path.join(args.save_path, save_name)
-        torch.save(net.state_dict(),save_path)
-        del full_loader
-if __name__ == '__main__':
-    args = get_args_parser()
-    args = args.parse_args()
-    args.device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
-    main(args)
+        main(args)
