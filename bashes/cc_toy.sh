b9f16eb25abab9f03ddb0ebd0a0ca2a31295e782
Joshua-Ren <renyi.joshua@gmail.com>
2023-05-03 10:48:36 -0700

    mpi3d exp

--- bashes/cc_toy.sh
+++ bashes/cc_toy.sh
@@ -25,11 +25,11 @@ cd /home/joshua52/projects/def-dsuth/joshua52/P4_Graph
 #srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_toy_3dshapes.py \
 #--config_file toy_nilsem
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_toy_3dshapes.py \
---config_file toy_semonly
-
 #srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_toy_3dshapes.py \
-#--config_file toy_nilonly
+#--config_file toy_semonly
+
+srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_toy_3dshapes.py \
+--config_file toy_nilonly
 
 #srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_toy_3dshapes.py \
 #--config_file toy_baseline
