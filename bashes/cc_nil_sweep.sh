e98cd3bcd7d3365899b74cd133809e578e635100
Joshua-Ren <renyi.joshua@gmail.com>
2023-05-14 15:09:24 -0700

    inital

---
+++ bashes/cc_nil_sweep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=def-dsuth
+#SBATCH --gres=1              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=4         # CPU cores/threads
+#SBATCH --mem=64000M               # memory per node
+#SBATCH --time=1-00:00            # time (DD-HH:MM)
+#SBATCH --output=./logs/stage1.txt 
+
+
+# 1. Load the required modules
+module load python/3.8
+
+# 2. Load your environment
+source /home/joshua52/projects/def-dsuth/joshua52/env_graph/bin/activate
+
+# 3. Copy your dataset on the compute node
+#cp /network/datasets/<dataset> $SLURM_TMPDIR
+
+# 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
+#    and look for the dataset into $SLURM_TMPDIR
+
+cd /home/joshua52/projects/def-dsuth/joshua52/P4_Graph
+
+wandb agent --count=5 joshuaren/P4_report/135gju6b
+
+
