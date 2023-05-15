597556b7fea9ffd6426a19c802d5f220fe6b0325
Joshua-Ren <renyi.joshua@gmail.com>
2023-05-15 16:32:06 -0700

    inital

--- bashes/cc_nil_run.sh
+++ bashes/cc_nil_run.sh
@@ -26,6 +26,9 @@ cd /home/joshua52/projects/def-dsuth/joshua52/P4_Graph
 #--config_file hiv_gcn_semnil_hard
 
 srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+--config_file pcba_gcn_baseline_hard
+
+srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
 --config_file pcba_gcn_baselineplus_hard
 
 srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
