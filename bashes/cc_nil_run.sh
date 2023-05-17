382ac4e07565290af40dd2f52c372340c8549a18
Joshua-Ren <renyi.joshua@gmail.com>
2023-05-16 23:26:37 -0700

    inital

--- bashes/cc_nil_run.sh
+++ bashes/cc_nil_run.sh
@@ -28,23 +28,23 @@ cd /home/joshua52/projects/def-dsuth/joshua52/P4_Graph
 # srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
 # --config_file pcba_gcn_baseline_hard
 
-# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
-# --config_file pcba_gcn_baselineplus_hard
+srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+--config_file pcba_gcn_baselineplus_hard
 
 # srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
 # --config_file pcba_gcn_semnil_hard
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
---config_file hiv_gcn_semnil_hard
+# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+# --config_file hiv_gcn_semnil_hard
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
---config_file hiv_gcn_baseline_hard
+# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+# --config_file hiv_gcn_baseline_hard
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
---config_file hiv_gcn_baseline_hard
+# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+# --config_file hiv_gcn_baseline_hard
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
---config_file hiv_gcn_sem_hard
+# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+# --config_file hiv_gcn_sem_hard
 
-srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
---config_file hiv_gcn_baselineplus_hard
+# srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_nil.py \
+# --config_file hiv_gcn_baselineplus_hard
