fa01f85e739de0b8f3cee51297a90489c8f80edc
Joshua-Ren <renyi.joshua@gmail.com>
2023-09-04 09:57:42 -0700

    new exp

--- bashes/cc_domnet.sh
+++ bashes/cc_domnet.sh
@@ -26,8 +26,8 @@ cd /home/joshua52/projects/def-dsuth/joshua52/P4_Graph
 srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_domainnet_celeba.py \
 --config_file domainnet_baseline
 
-#srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_domainnet_celeba.py \
-#--config_file domainnet_mile
+srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_domainnet_celeba.py \
+--config_file domainnet_mile
 
 #srun python /home/joshua52/projects/def-dsuth/joshua52/P4_Graph/main_domainnet_celeba.py \
 #--config_file domainnet_mile
